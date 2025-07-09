996710369dc4ee9622df51cc98bc0dadb72436c4
pdrov <hsvictor990@gmail.com>
2025-07-09 15:36:13 -0300

    Atualizando exercíos com módulos

---
+++ src/exer.ts
@@ -0,0 +1,28 @@
+//Exercício 01
+//Crie uma função que retorne a string "Olá, mundo!".
+
+export function olaMundo(): string {
+    return "Olá, mundo!";
+}
+
+//Exercício 02
+//A função recebe um parâmetro do tipo number e retorna o resultado da
+//multiplicação por 2.
+
+function multiplicacao(numero1: number): number {
+    return numero1 * 2;
+}
+
+export const resultado: number = multiplicacao(3);
+
+
+//Exercício 03
+//Crie uma função que recebe duas palavras (strings) e retorna sua concatenação. De forma resumida
+//deve juntar as duas palavras
+
+function nomeCompleto(nome: string, sobrenome: string): string {
+    return nome + " " + sobrenome;
+}
+
+
+export const nomeInteiro: string = nomeCompleto("Lucas", "Ferreira");
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,7 @@
-//Exercício 01
-//Crie uma função que retorne a string "Olá, mundo!".
-
-function olaMundo(): string {
-    return "Olá, mundo!"
-}
+import { olaMundo, resultado, nomeInteiro } from "./exer";
 
 (function main(): void {
     console.log(olaMundo());
+    console.log(`O resultado da multiplicação é: ${resultado}.`);
+    console.log(`O nome completo da pessoa é: ${nomeInteiro}.`);
 })();
-
-//Exercício 02
-//A função recebe um parâmetro do tipo number e retorna o resultado da
-//multiplicação por 2.
-
-function multiplicacao(numero1: number): number {
-    return numero1 * 2;
-}
-
-const resultado: number = multiplicacao(3);
-console.log(`O resultado da multiplicação é:  ${resultado}.`);
-
-//Exercício 03
-//Crie uma função que recebe duas palavras (strings) e retorna sua concatenação. De forma resumida
-//deve juntar as duas palavras
-
-function nomeCompleto(nome: string, sobrenome: string): string {
-    return nome + " " + sobrenome;
-}
-
-const nomeInteiro: string = nomeCompleto("Lucas", "Ferreira");
-console.log(`O nome completo da pessoa é: ${nomeInteiro}`);
