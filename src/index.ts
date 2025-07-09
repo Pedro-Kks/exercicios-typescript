ba4be071091564b5aa4fa1a5710593d418f99c39
pdrov <hsvictor990@gmail.com>
2025-07-09 17:47:45 -0300

    Nova mudança de exercicios

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-import { olaMundo, resultado, nomeInteiro } from "./exer";
+//Aplicando módulos em TypeScript para evitar escopo global usando export e import
 
+import { olaMundo, resultado, nomeInteiro } from "./exer.js";
+import { exibicao } from "./bitexer.js";
+
+
+//Função main para exercícios do arquivo exer.ts (curso)
 (function main(): void {
     console.log(olaMundo());
     console.log(`O resultado da multiplicação é: ${resultado}.`);
     console.log(`O nome completo da pessoa é: ${nomeInteiro}.`);
 })();
+
+
+
+
+//Exercícios da bitexer.tsp
+console.log(exibicao(' ', ' ', ' ', 0))
